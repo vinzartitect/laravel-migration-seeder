17a0ca89b6f568625309d2de1feaadd5a34051cd
VincenzoBorriello89 <vincborr89@gmail.com>
2022-05-26 20:08:55 +0200

    inserimento a schermo delle date

--- resources/views/pages/trains/show.blade.php
+++ resources/views/pages/trains/show.blade.php
@@ -9,6 +9,7 @@
                 <h5 class="card-title">{{ $train -> Azienda }}</h5>
                 <h6 class="card-subtitle mb-2 text-muted">{{ $train -> StazionePartenza }}</h6>
                 <h6 class="card-subtitle mb-2 text-muted">{{ $train -> StazioneArrivo }}</h6>
+                <h6 class="card-subtitle mb-2 text-muted">{{ $train -> GiornoPartenza }}</h6>
                 <h6 class="card-subtitle mb-2 text-muted">{{ $train -> OrarioPartenza }}</h6>
                 <h6 class="card-subtitle mb-2 text-muted">{{ $train -> OrarioArrivo }}</h6>
                 <h6 class="card-subtitle mb-2 text-muted">{{ $train -> CodiceTreno }}</h6>
